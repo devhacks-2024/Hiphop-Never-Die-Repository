82df2aa96a45c95ee234113df1748a583a83bc73
Peter Vu <dmtruong1980@gmail.com>
2024-02-24 09:16:46 -0600

    chore: Save changes to Djikstra

--- scripts/dijkstra.js
+++ scripts/dijkstra.js
@@ -1,26 +1,37 @@
-
 // Javascript program to get least cost path in a grid from
 // top-left to bottom-right
-var ROW = 5
-var COL = 5
+const ROW_COUNT = 50;
+const COL_COUNT = 50;
+const LAYER_COUNT = 6;
 
-// structure for information of each cell
-class cell
-{
-  constructor(x, y, distance)
-  {
+// structure for information of each Cell
+class Cell {
+  constructor(x, y, distance) {
     this.x = x;
     this.y = y;
     this.distance = distance;
   }
-};
 
+  constructor(x, y, z, distance) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.distance = distance;
+  }
+}
 
 // Utility method to check whether a point is
 // inside the grid or not
 function check(i, j, grid, x1, y1, x2, y2)
 {
-  return (i >= 0 && i < ROW && j >= 0 && j < COL && grid[i][j] != 0);
+  return (i >= 0 && i < ROW_COUNT && j >= 0 && j < COL_COUNT && grid[i][j] != 0);
+}
+
+function checkInGrid(i, j, k, arr, x1, y1, z1, x2, y2, z2) {
+  return (i >= 0 && i < ROW_COUNT
+    && j >= 0 && j < COL_COUNT
+    && k >= 0 && k < LAYER_COUNT
+    && grid[k][i][j] !== 0);
 }
 
 
@@ -40,8 +51,8 @@ function shortest(grid, row, col, x1, y1, x2, y2)
 
   var st = [];
 
-  // insert (0, 0) cell with 0 distance
-  st.push(new cell(x1, y1, 0));
+  // insert (0, 0) Cell with 0 distance
+  st.push(new Cell(x1, y1, 0));
 
   // initialize distance of (0, 0) with its grid value
   dis[x1][y1] = grid[x1][y1];
@@ -49,7 +60,7 @@ function shortest(grid, row, col, x1, y1, x2, y2)
   // loop for standard dijkstra's algorithm
   while (st.length!=0)
   {
-    // get the cell with minimum distance and delete
+    // get the Cell with minimum distance and delete
     // it from the set
     var k = st[0];
     st.shift();
@@ -64,14 +75,14 @@ function shortest(grid, row, col, x1, y1, x2, y2)
       if (!check(x, y, grid))
         continue;
 
-      // If distance from current cell is smaller, then
-      // update distance of neighbour cell
+      // If distance from current Cell is smaller, then
+      // update distance of neighbour Cell
       if (dis[x][y] > dis[k.x][k.y] + grid[x][y])
       {
         // update the distance and insert new updated
-        // cell in set
+        // Cell in set
         dis[x][y] = dis[k.x][k.y] + grid[x][y];
-        st.push(new cell(x, y, dis[x][y]));
+        st.push(new Cell(x, y, dis[x][y]));
       }
     }
     st.sort((a,b)=>{
@@ -90,7 +101,7 @@ function shortest(grid, row, col, x1, y1, x2, y2)
   //   for(let j = 0; j < 5; ++j)
   //     document.write(dis[i][j] + " ");
   // uncomment below code to print distance
-  // of each cell from (0, 0)
+  // of each Cell from (0, 0)
   let  pos = [x2, y2];
   while(pos[0] != x1 || pos[1] != y1)
   {
@@ -124,20 +135,26 @@ function shortest(grid, row, col, x1, y1, x2, y2)
    }
   
   // dis[row - 1][col - 1] will represent final
-  // distance of bottom right cell from top left cell
+  // distance of bottom right Cell from top left Cell
   return dis[x2][y2];
 }
 
+function shortestPath(arr, floor, row, col, x1, y1, z1, x2, y2, z2) {
+  let dist = Array.from(Array(floor), () => Array.from(Array(row), ()=>Array(col).fill(0)))
+
+  console.log(dist);
+}
+
 // Driver code to test above methods
-var grid =
-[
-  [1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 0],
-  [1, 1, 1, 1, 0],
-  [1, 1, 0, 1, 0],
-  [0, 1, 0, 1, 1]
-];
-document.write(shortest(grid, ROW, COL, 1, 0, 3, 3));
+// var grid =
+// [
+//   [1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 0],
+//   [1, 1, 1, 1, 0],
+//   [1, 1, 0, 1, 0],
+//   [0, 1, 0, 1, 1]
+// ];
+// document.write(shortest(grid, ROW_COUNT, COL_COUNT, 1, 0, 3, 3));
 
 // This code is contributed by rutvik_56.
 
