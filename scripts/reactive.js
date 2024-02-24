db7846d67c468b5fb9164c61b6b0788920595862
Sam Heppner <s.heppner0825@gmail.com>
2024-02-24 09:14:00 -0600

    feat: grid overlay test

--- scripts/reactive.js
+++ scripts/reactive.js
@@ -1,16 +1,31 @@
+import map from "../data/map1.json" assert { type: "json"};
+
 var floor = 2;
 
-changeFloorUp = function() {
+const changeFloorUp = function() {
     if (floor < 6) {
         floor++;
-        document.getElementById("map-image").src = "assets/img/floor" + floor + ".png";
+        document.getElementById("map-image").src = "assets/img/floor_" + floor + ".png";
+
+        updateTable();
     }
 }
 
-changeFloorDown = function() {
+const changeFloorDown = function() {
     if (floor > 1) {
         floor--;
-        document.getElementById("map-image").src = "asssets/img/floor" + floor + ".png";
+        document.getElementById("map-image").src = "assets/img/floor_" + floor + ".png";
+
+        updateTable();
+    }
+}
+
+function updateTable() {
+    for (let i = 0; i < 50; i++) {
+        for (let j = 0; j < 50; j++) {
+            document.getElementById("cell" + ((50 * i) + j)).innerHTML = 
+                map.content[floor - 1][i][j] == 1 ? "." : "";
+        }
     }
 }
 
@@ -25,4 +40,20 @@ window.onload = () => {
 
     document.getElementById("floor-up").addEventListener("click", changeFloorUp);
     document.getElementById("floor-down").addEventListener("click", changeFloorDown); 
+
+    let table = document.getElementById("grid");
+    let tbody = document.createElement("tbody");
+
+    for (let i = 0; i < 50; i++) {
+        let row = document.createElement("tr");
+        row.id = "row" + i;
+        for (let j = 0; j < 50; j++) {
+            let cell = document.createElement("td");
+            cell.id = "cell" + ((50 * i) + j);
+            cell.innerHTML = map.content[floor - 1][i][j];
+            row.appendChild(cell);
+        }
+        tbody.appendChild(row);
+    }
+    table.appendChild(tbody);
 }
